b193888e9e357c2773b5903d1f19a30a7d5e4e74
sid <abhinavkilam@gmail.com>
2025-09-25 13:15:57 -0500

    Added if statement to convert grade into GPA is sudent got an A

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,6 +5,9 @@ Grade = int(input("What grade did you get in your class?"))
 
 #GPA Conversion for As
 
+if Grade >= 90 and Grade <= 100:
+    print("Your GPA is 4.0")
+
 #GPA Conversion for Bs
 
 #GPA Conversion for Cs
